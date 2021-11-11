68db749c6c6290667da5dd0cdfadd0e3eba0977e
Richard <einetunet@gmail.com>
2021-11-11 23:35:24 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const PORT = 8000;
 
 const server = serve({ port: PORT });
 
-const body = new TextEncoder().encode("Hello World from Deno! " + "(" + Date.toString() + ")\n");
+const body = new TextEncoder().encode("Hello World from Deno! " + "(" + new Date().toString() + ")\n");
 
 console.log(`Server started on port ${PORT}`);
 
