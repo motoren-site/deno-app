ab4a8666b51007586c87a7adc6d249a663b15532
Richard Haugen Olsen <richard.haugen.olsen@miles.no>
2023-09-24 20:46:54 +0200

    New deployment

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ const PORT = 8000;
 
 const server = serve({ port: PORT });
 
-
-
 console.log(`Server started on port ${PORT}`);
 
-
 for await (const req of server) {
-  const body = new TextEncoder().encode("Hello World from Deno !! " + "(" + new Date().toString() + ")\n");
+  const body = new TextEncoder().encode(
+    "Hello World from Deno ! " + "(" + new Date().toString() + ")\n"
+  );
   req.respond({ body });
 }
